8905e9f58509e9b61e86fcfa2475737cc3550a9e
sergiovelmay <sergiovelmay@gmail.com>
2020-01-29 21:01:08 +0100

    Listing1-7 project added.

---
+++ Chapter1/Listing1-7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+/*
+ * LISTING 1-7 Queuing some work to the thread pool
+ */
+
+namespace Chapter1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            ThreadPool.QueueUserWorkItem((s) => // The threads are sended back to the pool where they can be reused whenever a request comes in.
+            {
+                Console.WriteLine("Thread_A_#{0} - Working on thread A from threadpool.", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            ThreadPool.QueueUserWorkItem((s) => // A queued work item is picked up by an available thread from the pool.
+            {
+                Console.WriteLine("Thread_B_#{0} - Working on thread B from threadpool.", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            ThreadPool.QueueUserWorkItem((s) => // The thread pool limits the available number of threads, you do get a lesser degree of parallelism than using the regular Thread class.
+            {
+                Console.WriteLine("Thread_C_#{0} - Working on thread C from threadpool.", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Console.WriteLine("Main_Thread_#{3} - ThreadPool Count: {2}, Completed: {0}, Pendig: {1}", ThreadPool.CompletedWorkItemCount, ThreadPool.PendingWorkItemCount, ThreadPool.ThreadCount, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
+
+/*
+CONSOLE:
+
+Thread_C_#6 - Working on thread C from threadpool.
+Thread_B_#4 - Working on thread B from threadpool.
+Main_Thread_#1 - ThreadPool Count: 4, Completed: 0, Pendig: 3
+Thread_A_#5 - Working on thread A from threadpool.
+ */
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # 70-483_Examples
-Examples for the Programming in C# Microsoft Exam Ref 70-483
+Examples for the Programming in C# Microsoft Exam Ref 70-483.
+
+## Chapter 1
+Manage program flow
+
+### Objective 1.1
+Implement multithreading and asynchronous processing
+
+#### Understanding threads
+LISTING 1-1 Creating a thread with the Thread class
+LISTING 1-2 Using a background thread
+LISTING 1-3 Using the ParameterizedThreadStart
+LISTING 1-4 Stopping a thread
+LISTING 1-5 Using the ThreadStaticAttribute
+LISTING 1-6 Using ThreadLocal<T>
+LISTING 1-7 Queuing some work to the thread pool
