5cfbfe3b8c4bce58212d0b5f64afdfaf92fba2bf
sergiovelmay <sergiovelmay@gmail.com>
2020-02-01 22:59:43 +0100

    Listing1-13 project added.

---
+++ Chapter1/Objective1.1/Listing1-013/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+
+/*
+ * LISTING 1-13 Using a TaskFactory
+ */
+
+namespace Listing1_013
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task<String[]> parent = Task.Run(() =>
+            {
+                Console.WriteLine("Parent Task running...");
+
+                var results = new String[3];
+
+                // A TaskFactory is created with a certain configuration. 
+                TaskFactory myTaskFactory = new TaskFactory(
+                    TaskCreationOptions.AttachedToParent,
+                    TaskContinuationOptions.ExecuteSynchronously
+                );
+
+                // Now, we can create Tasks with the same configuration.
+                myTaskFactory.StartNew(() => {
+                    Console.WriteLine("1st child Task running...");
+                    results[0] = "1st child Task result OK.";
+                });
+                myTaskFactory.StartNew(() => {
+                    Console.WriteLine("2nd child Task running...");
+                    results[1] = "2nd child Task result OK.";
+                });
+                myTaskFactory.StartNew(() => {
+                    Console.WriteLine("3rd child Task running...");
+                    results[2] = "3rd child Task result OK.";
+                });
+
+                return results;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask => {
+                    foreach (string taskEnded in parentTask.Result)
+                    {
+                        Console.WriteLine(taskEnded);
+                    }
+                }
+            );
+
+            finalTask.Wait();
+
+            Console.WriteLine("Parent Task completed.");
+        }
+    }
+}
+
+/*
+CONSOLE:
+
+Parent Task running...
+1st child Task running...
+2nd child Task running...
+3rd child Task running...
+1st child Task result OK.
+2nd child Task result OK.
+3rd child Task result OK.
+Parent Task completed.
+*/
