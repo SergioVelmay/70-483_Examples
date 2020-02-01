47005ec33acf08b663525ed5c27304f49fb75f28
sergiovelmay <sergiovelmay@gmail.com>
2020-02-01 16:15:03 +0100

    Listing1-8 project added.

---
+++ Chapter1/Listing1-8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/*
+ * LISTING 1-8 Starting a new Task
+ */
+
+namespace Chapter1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task mytask = Task.Run(() => // A Task represents some work that should be done.
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    // The Task scheduler uses threads from the thread pool to execute the Task.
+                    Console.Write("Thread {0} - ", Thread.CurrentThread.ManagedThreadId);
+
+                    for (int y = x; y < 9; y++)
+                    {
+                        Console.Write('*');
+                    }
+
+                    Console.WriteLine();
+                }
+            });
+
+            Console.WriteLine("Thread {0} - TASK START", Thread.CurrentThread.ManagedThreadId);
+
+            mytask.Wait(); // Start a new Task and wait until it’s finished.
+
+            Console.WriteLine("Thread {0} - TASK END", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
+/*
+CONSOLE:
+
+Thread 1 - TASK START
+Thread 4 - *********
+Thread 4 - ********
+Thread 4 - *******
+Thread 4 - ******
+Thread 4 - *****
+Thread 4 - ****
+Thread 4 - ***
+Thread 4 - **
+Thread 4 - *
+Thread 1 - TASK END
+*/
