637481fa58bb89d5d0ad23a2150e791942fd124b
sergiovelmay <sergiovelmay@gmail.com>
2020-02-15 13:41:02 +0100

    Listing1-18 project added.

---
+++ Chapter1/Objective1.1/Listing1-018/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+/*
+ * LISTING 1-18 Async and Await
+ */
+
+namespace Listing1_018
+{
+    public static class Program
+    {
+        // The entry method of an application can’t be marked as async.
+        // Use the .Wait method in Main instead of the await keyword.
+        public static void Main()
+        {
+            Console.WriteLine("\nMain Thread START ------------\n");
+
+            string result = DownloadContent().Result;
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("\nMain Thread END ------------\n");
+        }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Console.WriteLine("\nAsync Task START ------\n");
+
+                // GetStringAsync uses asynchronous code internally and returns a Task<string>.
+                string result = await client.GetStringAsync("https://sergiovelmay.com");
+
+                Console.WriteLine("\nAsync Task END ------\n");
+
+                return result;
+            }
+        }
+    }
+}
+
+/*
+CONSOLE:
+
+Main Thread START ------------
+
+Async Task START ------
+
+Async Task END ------
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+</head>
+<body>
+</body>
+</html>
+
+Main Thread END ------------
+*/
+
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Examples from the Programming in C# Microsoft Exam Ref 70-483 Book written by Wo
 
 ### Objective 1.1. Implement multithreading and asynchronous processing
 
-#### Understanding threads
+#### A. Understanding Threads
 - LISTING 1-1 Creating a thread with the Thread class
 - LISTING 1-2 Using a background thread
 - LISTING 1-3 Using the ParameterizedThreadStart
@@ -14,7 +14,7 @@ Examples from the Programming in C# Microsoft Exam Ref 70-483 Book written by Wo
 - LISTING 1-6 Using ThreadLocal
 - LISTING 1-7 Queuing some work to the thread pool
 
-#### Using Tasks
+#### B. Using Tasks
 - LISTING 1-8 Starting a new Task
 - LISTING 1-9 Using a Task that returns a value
 - LISTING 1-10 Adding a continuation
@@ -24,8 +24,9 @@ Examples from the Programming in C# Microsoft Exam Ref 70-483 Book written by Wo
 - LISTING 1-14 Using Task.WaitAll
 - LISTING 1-15 Using Task.WaitAny
 
-#### Using the Parallel class
+#### C. Using the Parallel class
 - LISTING 1-16 Using Parallel.For and Parallel.ForEach
 - LISTING 1-17 Using Parallel.Break
 
-#### Using async and await
+#### D. Using Async and Await
+- LISTING 1-18 Async and Await
